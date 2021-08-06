3f78fbc180e2fe90f2b2e979586c7b17a8cb0dda
Andrew Massey <acmassey@ncsu.edu>
2021-08-06 13:15:02 -0400

    Create strong tests for encrypt method

---
+++ lib/enigma.rb
@@ -0,0 +1,25 @@
+class Enigma
+
+  def initialize
+
+  end
+
+  def encrypt(message, key = random_key, date = today)
+    message
+  end
+
+  def random_key
+    key = rand(99999).to_s
+
+    if key.length < 5
+      key.prepend("0") until key.length == 5
+      end
+    key
+  end
+
+  def today(date = Time.now)
+    date.strftime("%d%m%y")
+  end
+
+
+end
