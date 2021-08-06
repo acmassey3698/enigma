d3997e7c1b155cc0473217d8c5e7ddc3a49c10f3
Andrew Massey <acmassey@ncsu.edu>
2021-08-06 13:23:55 -0400

    Adjust enigma to include generateable

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,21 +1,24 @@
+require "./key"
+
 class Enigma
+include Generateable
 
   def initialize
 
   end
 
-  def encrypt(message, key = random_key, date = today)
+  def encrypt(message, key = generate_key, date = today)
     message
   end
 
-  def random_key
-    key = rand(99999).to_s
-
-    if key.length < 5
-      key.prepend("0") until key.length == 5
-      end
-    key
-  end
+  # def random_key
+  #   key = rand(99999).to_s
+  #
+  #   if key.length < 5
+  #     key.prepend("0") until key.length == 5
+  #     end
+  #   key
+  # end
 
   def today(date = Time.now)
     date.strftime("%d%m%y")
