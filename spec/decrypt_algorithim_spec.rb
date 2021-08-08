9f0b44ab11b627f8c1a1a00bfa71cb8ce4a4145a
Andrew Massey <acmassey@ncsu.edu>
2021-08-08 17:25:06 -0400

    Complete methods and tests for the decrypt algorithim class

--- spec/decrypt_algorithim_spec.rb
+++ spec/decrypt_algorithim_spec.rb
@@ -3,3 +3,34 @@ SimpleCov.start
 require "./lib/generateable"
 require "./lib/enigma"
 require "./lib/decrypt_algorithim"
+
+RSpec.describe DecryptAlgorithim do
+
+  it "exists with attributes" do
+    algo = DecryptAlgorithim.new("keder ohulw", "02715", "040895")
+
+    expect(algo).to be_a(DecryptAlgorithim)
+    expect(algo.ciphertext).to eq("keder ohulw")
+    expect(algo.key).to eq("02715")
+    expect(algo.date).to eq("040895")
+  end
+
+  it "creates a message" do
+    algo = DecryptAlgorithim.new("keder ohulw", "02715", "040895")
+    algo_2 = DecryptAlgorithim.new("taqyhpvkhqublz", "05361", "080522")
+
+    expect(algo.create_message).to eq("hello world")
+    expect(algo_2.create_message).to eq("my name andrew")
+  end
+
+  it "decrypts a character" do
+    algo = DecryptAlgorithim.new("keder ohulw", "02715", "040895")
+    algo_2 = DecryptAlgorithim.new("taqyhpvkhqublz", "05361", "080522")
+
+    expect(algo.decrypt_character("k", 0, [3, 27, 73, 20])).to eq("h")
+    expect(algo_2.decrypt_character("a", 1, [7, 57, 44, 65])).to eq("y")
+  end
+
+
+
+end
