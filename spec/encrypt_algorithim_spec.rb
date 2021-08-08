9d604d7904d97d44b7c94691c4386a47f8f7e8fd
Andrew Massey <acmassey@ncsu.edu>
2021-08-08 16:05:20 -0400

    Create stronger tests for the encrypt algorithim

--- spec/encrypt_algorithim_spec.rb
+++ spec/encrypt_algorithim_spec.rb
@@ -15,17 +15,19 @@ RSpec.describe EncryptAlgorithim do
     expect(algo.date).to eq("040895")
   end
 
-  it "completes a rotation" do
-    algo = EncryptAlgorithim.new("Hello World", "02715", "040895")
+  it "creates a message" do
+    algo   = EncryptAlgorithim.new("Hello World", "02715", "040895")
     algo_2 = EncryptAlgorithim.new("My Name Andrew", "05361", "080522")
 
     expect(algo.create_message).to eq("keder ohulw")
     expect(algo_2.create_message).to eq("taqyhpvkhqublz")
   end
 
-  it "encrypts a character" do
-    algo = EncryptAlgorithim.new("Hello World", "02715", "040895")
+  it "encrypts each character" do
+    algo   = EncryptAlgorithim.new("Hello World", "02715", "040895")
+    algo_2 = EncryptAlgorithim.new("My Name Andrew", "05361", "080522")
 
     expect(algo.encrypt_character("h", 0, [3, 27, 73, 20])).to eq("k")
+    expect(algo_2.encrypt_character("y", 1, [7, 57, 44, 65])).to eq("a")
   end
 end
