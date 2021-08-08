f8b55e05d6bb5c76ddf2af3d3a67215cf5435005
Andrew Massey <acmassey@ncsu.edu>
2021-08-08 17:25:40 -0400

    Update Enigma class to have a decrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,14 +1,17 @@
 require "./generateable"
 require "date"
+require "./encrypt_algorithim"
+require "./decrypt_algorithim"
 
 class Enigma
 include Generateable
 
-  def initialize
-
-  end
-
   def encrypt(message, key = random_key, date = Date.today.strftime("%d%m%y"))
+    if key.length == 6
+      date = key
+      key = random_key
+    end
+
     algorithim = EncryptAlgorithim.new(message, key, date)
     output = {
       :encryption => algorithim.create_message,
