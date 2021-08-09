7e4f0aeb2f98a2b4bbf7b5066534d4173d3df8bd
Andrew Massey <acmassey@ncsu.edu>
2021-08-09 15:30:02 -0400

    Restructure the final_shifts method to return a hash

--- lib/generateable.rb
+++ lib/generateable.rb
@@ -25,10 +25,10 @@ module Generateable
   end
 
   def final_shifts(key, date)
-    [(split_keys(key)[0].to_i + offsets(date)[0].to_i),
-     (split_keys(key)[1].to_i + offsets(date)[1].to_i),
-     (split_keys(key)[2].to_i + offsets(date)[2].to_i),
-     (split_keys(key)[3].to_i + offsets(date)[3].to_i)]
+    {:a =>(split_keys(key)[0].to_i + offsets(date)[0].to_i),
+     :b =>(split_keys(key)[1].to_i + offsets(date)[1].to_i),
+     :c =>(split_keys(key)[2].to_i + offsets(date)[2].to_i),
+     :d =>(split_keys(key)[3].to_i + offsets(date)[3].to_i)}
   end
 
   def characters_array
